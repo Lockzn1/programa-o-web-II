55176874efc2d329a66f984e928a649f79a893d1
Murilo Cotrim <cotrim.murilo@gmail.com>
2024-08-29 22:28:55 -0300

    Aprendendo framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Laravel</title>
+    </head>
+    <body>
+
+        <h1>Olá Mundo!</h1>
+        <ul>
+            <li>
+                <a href="contato">Contato</a>
+                <br></li>
+            <li>
+                <a href="quemsomos">Quem somos?</a>
+            </li>
+        </ul>
+
+    </body>
+</html>
